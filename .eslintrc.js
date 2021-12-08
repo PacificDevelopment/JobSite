65b0c1233e28a53055c59e66b20ef5a38a5aa91d
Thistleman <codes5096@gmail.com>
2021-12-08 12:29:15 -0800

    Implemented eslint file updates and npm init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    "react/prop-types": 0,
+  },
+
+};
