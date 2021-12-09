5ac3c982af12435b2e4ad5526c21ff52d14f609e
Kevin Goble <goble.kevin@gmail.com>
2021-12-09 12:10:54 -0800

    Sets up React-Refresh, Webpack Hot Module Replacment, and React-Refresh-Webpack-Plugin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <h1>Hello JobSeekers</h1>;
+  return <h2>Hello JobSeekers</h2>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,13 +14,23 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist')
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'JobSite',
       template: path.resolve(__dirname, './src/index.html'),
     }),
     new CleanWebpackPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ],
+  mode: 'development',
   module: {
     rules: [
       // JavaScript
@@ -30,6 +41,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['react-refresh/babel']
           },
         },
       },
