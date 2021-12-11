afa43ec2effab03684185fce0a27f1d1bba3e4d5
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2021-12-10 16:28:19 -0800

    Create models.oneclick.js
    Created queries to get user info from the database and update the applications table
    
    Co-Authored-By: ahjohnston <78059363+ahjohnston@users.noreply.github.com>

---
+++ server/models.oneclick.js
@@ -0,0 +1,69 @@
+const axios = require('axios');
+
+//front-end call will look like
+//axios call to url:port
+
+
+//server call will look like:
+//listen at port
+//app.get / app.post
+//invoke queryTheDatabase(ids)
+
+app.post('/oneclick', (req, res) => {
+  let reqdata = req.body;
+  res.send('Hello Job Seekers!')
+})
+
+//database model will look like:
+//pool.connect connects to the postres database
+//const queryTheDatabase = pool.query(query string, blah blah blah)
+/*
+Queries Job Seeker table --> return seekerID
+Queries Job Seeker table (or resume table) -->
+return the saved resume and saved CL
+Queries Job Listing table --> return employerID
+*/
+
+async (session_id, job_post_id) => {
+  let user = await pool.query('SELECT user_id FROM users WHERE session_id = $1',[session_id]);
+  let resume = await pool.query('SELECT id FROM resumes WHERE user_id = $1', [user]);
+  let coverLetter = await pool.query('SELECT id FROM cover_letters WHERE user_id = $1', [user]);
+  let employerID = await pool.query('SELECT employer_id FROM job_posts WHERE id = $1', [job_post_id]);
+
+  pool.query(`INSERT into Applications (employer_id, cover_letter_id, resume_id, job_post_id, user_id) values ($1, $2, $3, $4, $5)`,
+  [employerID, coverLetter, resume, job_post_id, user])
+  .then((data) => console.log('WE ARE MASTERS'), data)
+  .catch((err) => console.log('WHYYYYY', err));
+}
+
+
+
+//add an alert if resume or cover letter does not exist, tell the user to upload?
+
+
+module.exports = {
+  getInfo: () => axios({
+    url: 'jdbc:postgresql://localhost/test',
+    method: 'get',
+  }),
+
+  postResume: (obj) => axios({
+    url: 'jdbc:postgresql://localhost/test',
+    method: 'post',
+    data: obj,
+  }),
+};
+
+/*
+const dbUser = require('../../dbconfig.js')
+const mongoose = require('mongoose');
+const mongoUri = `mongodb://${dbUser.username}:${dbUser.pwd}@3.19.74.12:27017/ProdReviews`;
+
+mongoose.connect(mongoUri, {
+  connectTimeoutMS: 60000
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
+*/
