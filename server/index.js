285fcc4c87ca2a29d9d0f64edcee606f60667d5d
Kevin Goble <goble.kevin@gmail.com>
2021-12-11 16:18:13 -0800

    Sets up backend structure

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const employers = require('./controllers/employersController');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.use(express.static('dist'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello Job Seekers!')
+  res.send('Hello Job Seekers!');
 })
 
+app.get('/data/employers', employers.retrieveEmployerData);
+
 app.listen(port, () => {
-  console.log(`Jobsite app listening at http://localhost:${port}`)
+  console.log(`Jobsite app listening at http://localhost:${port}`);
 })
