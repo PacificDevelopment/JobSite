aaa0cbfb0693a7682adcbf83005db6f7187406c6
Samuel Dudley <sbarkerdudley@gmail.com>
2021-12-13 12:15:25 -0800

    Fixed eslint issues - added rule for ract function definitions

--- src/app/components/jobs/JobSearchDrawer.jsx
+++ src/app/components/jobs/JobSearchDrawer.jsx
@@ -1,33 +1,35 @@
 import React, { useContext } from 'react';
-import { Drawer, Button, Box } from '@mui/material';
-import JobSearchParams from './JobSearchParams.jsx';
-import { JobSearchContext } from './JobSearchContext.jsx';
-
+import {
+  Drawer, Button, Box, Typography,
+} from '@mui/material';
+import JobSearchParams from './JobSearchParams';
+import { JobSearchContext } from './JobSearchContext';
 
 const JobSearchDrawer = () => {
-
-  let { drawer, setDrawer, query, setQuery } = useContext(JobSearchContext);
-  let toggleDrawer = () => setDrawer(!drawer);
-  let updateQuery = () => {}
+  const {
+    drawer, setDrawer,
+  } = useContext(JobSearchContext);
+  const toggleDrawer = () => setDrawer((d) => !d);
+  const updateQuery = () => { };
 
   return (
     <>
       {/* <Button onClick={toggleDrawer} fullWidth >Search Filters</Button> */}
-      <Button onClick={updateQuery} fullWidth >Search</Button>
-        <Drawer
-          anchor='right'
-          variant='temporary'
-          open={drawer}
-          onClose={toggleDrawer}
-        >
-      <Box role='presentation' sx={{width: 250, m: 2}}>
-          <h1>Filters</h1>
+      <Button onClick={updateQuery} sx={{ background: 'black' }} fullWidth>Search</Button>
+      <Drawer
+        anchor="right"
+        variant="temporary"
+        open={drawer}
+        onClose={toggleDrawer}
+      >
+        <Box role="presentation" sx={{ width: 250, m: 2 }}>
+          <Typography>Filters</Typography>
           <JobSearchParams />
-          <Button onClick={updateQuery} label='Apply Filters'/>
-      </Box>
-        </Drawer>
+          <Button onClick={updateQuery} label="Apply Filters" />
+        </Box>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
 export default JobSearchDrawer;
--- src/app/components/jobs/JobSearchParams.jsx
+++ src/app/components/jobs/JobSearchParams.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Stack, Button, Container } from '@mui/material';
-import { SortBy, Range, Experience, EmploymentType } from './RadioGroups.jsx';
+import {
+  SortBy, Range, Experience, EmploymentType,
+} from './RadioGroups.jsx';
 import SalarySlider from './SalarySlider.jsx';
 
-
 const JobSearchParams = () => {
-
   return (
     <Container>
       <Stack>
@@ -14,11 +14,10 @@ const JobSearchParams = () => {
         <Experience />
         <EmploymentType />
         <SalarySlider />
-        <Button onClick={''} label='Apply Filters' />
+        <Button onClick="" label="Apply Filters" />
       </Stack>
     </Container>
   );
-}
-
+};
 
 export default JobSearchParams;
