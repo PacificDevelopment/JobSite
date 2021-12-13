a86bd4d169519eb57aa54b096e285e159565f4ce
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2021-12-13 13:54:43 -0800

    list/focus item display functionality

--- src/app/JobsList/JobItem.jsx
+++ src/app/JobsList/JobItem.jsx
@@ -1,12 +1,29 @@
-// import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 
+
 const JobItem = (props) => {
+  const [displayToggle, setToggle] = useState(true);
+
+  const jobSelect = (selectedJob) => {
+    props.handleFocus(selectedJob)
+    console.log('what is up yall', selectedJob);
+  }
+  useEffect(() => {
+    console.log('useEffect in jobItem');
+    if (displayToggle) {
+      props.handleFocus(props.job);
+      setToggle(false);
+    }
+
+  },[]);
+
   return (
 
       <Card >
@@ -25,6 +42,10 @@ const JobItem = (props) => {
           <br />
           {props.job.date}
         </Typography>
+        <CardActions>
+        <Button size="small" onClick={(e) => {jobSelect(props.job)}}>Learn More</Button>
+        </CardActions>
+
         </CardContent>
       </Card>
 
