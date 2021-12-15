ffd99566998c36249d559b087af94db5b54d2843
Kevin Goble <goble.kevin@gmail.com>
2021-12-15 13:29:18 -0800

    Save Job Button posts job

--- src/app/components/SaveJobButton/SaveJobButton.jsx
+++ src/app/components/SaveJobButton/SaveJobButton.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import PrimaryButton from '../PrimaryButton';
 
 const SaveJobButton = ({job}) => {
-
+  const handleSaveJobClick = (e) => {
+    axios.post('/savejob', {
+      data: job,
+    })
+  }
   return(
-    <PrimaryButton text={'Save Job'} />
+    <PrimaryButton text={'Save Job'} onClick={handleSaveJobClick} />
   )
 };
 
