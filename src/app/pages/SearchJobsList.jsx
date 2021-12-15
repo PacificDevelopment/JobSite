5399c23d19077c4d0cc905379b03bc95d513e6b4
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2021-12-15 10:21:34 -0800

    premerge commit

--- src/app/pages/SearchJobsList.jsx
+++ src/app/pages/SearchJobsList.jsx
@@ -3,12 +3,17 @@ import { useState, useEffect} from 'react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import JobsList from "../jobs/JobSearch.jsx";
+import JobsSearch from "../jobs/JobSearch.jsx";
 
 import Main from "../JobsList/Main.jsx";
 
 const SearchJobsList () => {
+
   return(
+    <JobSearch/>
+    <Main/>
 
   )
 }
+
+export default SearchJobsList;
