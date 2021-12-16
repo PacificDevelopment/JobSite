9d80d7cc9d2e02a4f2c9e1aa1c535396e2473b39
Kevin Goble <goble.kevin@gmail.com>
2021-12-16 12:10:23 -0800

    Refactor for clarity and code standards

--- src/app/components/SaveJobButton/SaveJobButton.jsx
+++ src/app/components/SaveJobButton/SaveJobButton.jsx
@@ -3,41 +3,39 @@ import axios from 'axios';
 import PrimaryButton from '../PrimaryButton';
 import SecondaryButton from '../SecondaryButton';
 
-const SaveJobButton = ({job}) => {
+function SaveJobButton({ job }) {
   const [saveStarted, setSaveStarted] = useState(false);
 
-  const handleSaveJobClick = (e) => {
+  const handleSaveJobClick = () => {
     setSaveStarted(true);
-  }
+  };
 
   const handleInterestLevelClick = (e) => {
-    job.interest_level = e.target.textContent;
+    const data = { job };
+    data.job.interest_level = e.target.textContent;
     axios({
       method: 'POST',
       url: '/saveJob',
-      withCredentials:true,
-      data: {
-        job: job,
-      },
-    })
+      withCredentials: true,
+      data,
+    });
     setSaveStarted(false);
-  }
+  };
 
   return (
     <div>
       { saveStarted
         ? (
-            <div>
-              <SecondaryButton text={'Interested'} onClick={handleInterestLevelClick} />
-              <SecondaryButton text={'Very Interested'} onClick={handleInterestLevelClick} />
-              <SecondaryButton text={'Extremely Interested'} onClick={handleInterestLevelClick} />
-            </div>
-          )
-        : null
-      }
-      <PrimaryButton text={'Save Job'} onClick={handleSaveJobClick} />
+          <div>
+            <SecondaryButton text="Interested" onClick={handleInterestLevelClick} />
+            <SecondaryButton text="Very Interested" onClick={handleInterestLevelClick} />
+            <SecondaryButton text="Extremely Interested" onClick={handleInterestLevelClick} />
+          </div>
+        )
+        : null}
+      <PrimaryButton text="Save Job" onClick={handleSaveJobClick} />
     </div>
   );
-};
+}
 
 export default SaveJobButton;
