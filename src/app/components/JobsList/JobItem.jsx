a26b4c951a66ca73c36747f7b7bda0b00ea82cb2
Quinton Maki <quintonmaki@gmail.com>
2021-12-16 13:47:16 -0800

    Finished job card styling

--- src/app/components/JobsList/JobItem.jsx
+++ src/app/components/JobsList/JobItem.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
+import React, { useState, useEffect } from 'react';
+import { Card, Box, Typography } from '@mui/material';
+import Interweave from 'interweave';
+import ta from 'time-ago';
 import SaveJobButton from '../SaveJobButton/SaveJobButton';
 import PrimaryButton from '../PrimaryButton';
 
-function JobItem({ handleFocus, job }) {
+function JobItem({ handleFocus, job, selected }) {
   const [displayToggle, setDisplayToggle] = useState(true);
 
   const jobSelect = (selectedJob) => {
@@ -23,25 +23,31 @@ function JobItem({ handleFocus, job }) {
     <Card
       variant="outlined"
       sx={{
-        mb: 2, position: 'relative', width: 450, p: 2, backgroundColor: '#EDFEFF',
+        mb: 2, position: 'relative', width: 450, p: 2, backgroundColor: '#EDFEFF', borderColor: '#9096A3', borderRadius: 2, borderWidth: selected ? 3 : 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
       }}
     >
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        {job.company}
-      </Typography>
-      <Typography variant="h5" component="div">
+      <Typography sx={{ mb: 1 }} variant="h5" component="div">
         {job.title}
+      </Typography>
+      <Typography sx={{ fontSize: 14, mb: 1, fontWeight: 700 }} color="text.primary" gutterBottom>
+        {job.company}
       </Typography>
       <Typography sx={{ mb: 1.5 }} color="text.secondary">
         {job.locations}
       </Typography>
       <Typography variant="body2">
-        {job.description}
-        <br />
-        {job.date}
+        <Interweave content={job.description} />
       </Typography>
-      <PrimaryButton text="Learn More" onClick={(e) => { jobSelect(job); }} />
-      <SaveJobButton job={job} />
+      <Box sx={{
+        display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 4,
+      }}
+      >
+        <Typography>
+          {ta.ago(job.date)}
+        </Typography>
+        <PrimaryButton sx={{ mb: 0, mt: 0 }} text="Learn More" onClick={(e) => { jobSelect(job); }} />
+        <SaveJobButton sx={{ mb: 0, mt: 0 }} job={job} />
+      </Box>
     </Card>
   );
 }
