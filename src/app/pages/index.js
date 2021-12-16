9ac82c1d032b6181f3b6f8c4e488f35fff10cbf6
Quinton Maki <quintonmaki@gmail.com>
2021-12-16 10:41:38 -0800

    Removed search route, converted over to dashboard

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -4,8 +4,7 @@ import SignUp from './SignUp';
 import Profile from './Profile';
 import Dashboard from './Dashboard';
 import Jobs from './Jobs';
-import SearchJobsList from './SearchJobsList';
 
 export {
-  Home, LogIn, SignUp, Profile, Dashboard, Jobs, SearchJobsList,
+  Home, LogIn, SignUp, Profile, Dashboard, Jobs,
 };
