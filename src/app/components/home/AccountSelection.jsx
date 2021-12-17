89a2a4e01aac1880da1003daa5dbb98139a984db
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2021-12-17 09:51:07 -0800

    Fixed sign-up bug where fields would not refresh when redirected to log-in

--- src/app/components/home/AccountSelection.jsx
+++ src/app/components/home/AccountSelection.jsx
@@ -3,30 +3,34 @@ import {
   Box, Paper, Typography,
 } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+
 import AuthButton from './AuthButton';
 import JobSearch from '../JobSearch/JobSearch';
 
 import PrimaryButton from '../PrimaryButton';
 
 function AccountSelection({ createAccount, newLogIn }) {
-  const [loginUsername, setLoginUsername] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-  const [registerUsername, setRegisterUsername] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
+  const [userField, setUserField] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const navigate = useNavigate();
 
   const register = () => {
     const curPort = location.port;
     Axios({
       method: 'POST',
       data: {
-        username: registerUsername,
-        password: registerPassword,
+        username: userField,
+        password: userPassword,
       },
       withCredentials: true,
       url: 'http://localhost:3000/register',
+    }).then(() => {
+      navigate('/login');
     });
+    setUserField('');
+    setUserPassword('');
   };
 
   const login = () => {
@@ -34,8 +38,8 @@ function AccountSelection({ createAccount, newLogIn }) {
     Axios({
       method: 'POST',
       data: {
-        username: loginUsername,
-        password: loginPassword,
+        username: userField,
+        password: userPassword,
       },
       withCredentials: true,
       url: 'http://localhost:3000/login',
@@ -103,9 +107,8 @@ function AccountSelection({ createAccount, newLogIn }) {
               label="Email address"
               sx={{ '&.Mui-focused': { color: '#4A485B', fontWeight: 700 } }}
               fullWidth
-              onChange={createAccount
-                ? (e) => setRegisterUsername(e.target.value)
-                : (e) => setLoginUsername(e.target.value)}
+              value={userField}
+              onChange={(e) => setUserField(e.target.value)}
             />
           </Paper>
           <Paper sx={{ mt: 2, mb: 1 }}>
@@ -113,9 +116,8 @@ function AccountSelection({ createAccount, newLogIn }) {
               label="Enter password"
               fullWidth
               sx={{ '&.Mui-focused': { color: '#4A485B', fontWeight: 700 } }}
-              onChange={createAccount
-                ? (e) => setRegisterPassword(e.target.value)
-                : (e) => setLoginPassword(e.target.value)}
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
             />
           </Paper>
           <PrimaryButton
