f09df74b5e113dbb14bac9a3edcc18be073077c7
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2021-12-16 20:32:41 -0800

    Implemented route protections on saved jobs and an alert to log-in

--- src/app/components/SaveJobButton/SaveJobButton.jsx
+++ src/app/components/SaveJobButton/SaveJobButton.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Box from '@mui/material/Box';
 import PrimaryButton from '../PrimaryButton';
 import SecondaryButton from '../SecondaryButton';
+import authUtils from '../../utils/authUtils';
 
 function SaveJobButton({ job, sx }) {
   const [saveStarted, setSaveStarted] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSaveJobClick = () => {
-    setSaveStarted(true);
+  const handleSaveJobClick = async () => {
+    let isLoggedIn = false;
+    await authUtils.getUser().then((res) => {
+      console.log('hsjk', res.data);
+      isLoggedIn = res.data.loggedIn;
+    });
+    isLoggedIn ? setSaveStarted(true)
+      : alert('You must be logged in to save jobs!'),
+    navigate('/');
   };
 
   const handleInterestLevelClick = (e) => {
