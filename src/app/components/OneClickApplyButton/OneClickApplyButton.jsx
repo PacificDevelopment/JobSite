37f4f9ee10edceb6fa8663046af324b219d75ef4
zacharia <zach0lentz@gmail.com>
2021-12-17 14:21:04 -0800

    added notification that appears when clicking apply

--- src/app/components/OneClickApplyButton/OneClickApplyButton.jsx
+++ src/app/components/OneClickApplyButton/OneClickApplyButton.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 import PrimaryButton from '../PrimaryButton';
 
 function OneClickApplyButton({ job, refreshJobs }) {
-  const handleOneClick = () => {
+  const [applyNotifyAnchor, setApplyNotifyAnchor] = useState(null);
+  const handleOneClick = (e) => {
     const data = { job };
+    setApplyNotifyAnchor(e.currentTarget);
     axios({
       method: 'POST',
       url: '/applyToJob',
       withCredentials: true,
       data,
     })
-      .then(() => refreshJobs(data.interest_level));
+      .then(() => {
+        if (refreshJobs) {
+          setTimeout(() => refreshJobs(data.interest_level), 1000);
+        } else {
+          setTimeout(() => setApplyNotifyAnchor(null), 1000);
+        }
+      });
   };
+  const handleClose = () => {
+    setApplyNotifyAnchor(null);
+  };
+  const open = Boolean(applyNotifyAnchor);
+  const popId = open ? 'simple-popover' : undefined;
 
   return (
-    <PrimaryButton text="One Click Apply" onClick={handleOneClick} />
+    <>
+      <PrimaryButton text="One Click Apply" onClick={handleOneClick} />
+      <Popover
+        id={popId}
+        open={open}
+        anchorEl={applyNotifyAnchor}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>Applied!</Typography>
+      </Popover>
+    </>
   );
 }
 
